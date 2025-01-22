359b6b86b6c8bc79157262c2d9d68124f58c2628
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-22 17:42:02 -0600

    working on mosaic design

--- client/src/components/MosaicPage.jsx
+++ client/src/components/MosaicPage.jsx
@@ -8,18 +8,18 @@ export default function MosaicPage({}) {
     <>
     <div className="mosaicContainer"> 
 
-    <div className="mosaicSquare square1"></div>
-    <div className="mosaicSquare square2"></div>
-    <div className="mosaicSquare square3"></div>
-    <div className="mosaicSquare square4"></div>
-    <div className="mosaicSquaresquare5"></div>
-    <div className="mosaicSquare square6"></div>
-    <div className="mosaicSquare square7"></div>
-    <div className="mosaicSquare square8"></div>
-    <div className="mosaicSquare square9"></div>
-    <div className="mosaicSquare square10"></div>
-    <div className="mosaicSquare square11"></div>
-    <div className="mosaicSquare square12"></div>
+    <div className="mosaicSquare square1">1</div>
+    <div className="mosaicSquare square2">2</div>
+    <div className="mosaicSquare square3">3</div>
+    <div className="mosaicSquare square4">4</div> 
+    <div className="mosaicSquare square5">5</div>
+    <div className="mosaicSquare square6">6</div>
+    <div className="mosaicSquare square7">7</div>
+    <div className="mosaicSquare square8">8</div>
+    <div className="mosaicSquare square9">9</div>
+    <div className="mosaicSquare square10">10</div>
+    <div className="mosaicSquare square11">11</div>
+    <div className="mosaicSquare square12">12</div>
 
     </div>
 
