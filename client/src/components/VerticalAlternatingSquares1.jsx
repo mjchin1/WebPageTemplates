28c582af0a7b040a2e2e98b0755f1f0f0cb71323
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-22 19:20:06 -0600

    adding new files

---
+++ client/src/components/VerticalAlternatingSquares1.jsx
@@ -0,0 +1,30 @@
+export default function VerticalAlternatingSquares({}) {
+
+
+
+
+
+  return (
+    <>
+    <div className="vas1Container"> 
+
+    <div className="vas1Square1 square">1</div>
+    <div className="vas1Square2 square">2</div>
+    <div className="vas1Square3 square">3</div>
+    <div className="vas1Square4 square">4</div> 
+    <div className="vas1Square5 square">5</div>
+    <div className="vas1Square6 square">6</div>
+    <div className="vas1Square7 square">7</div>
+    <div className="vas1Square8 square">8</div>
+    <div className="vas1Square9 square">9</div>
+    <div className="vas1Square10 square">10</div>
+    <div className="vas1Square11 square">11</div>
+    <div className="vas1Square12 square">12</div>
+
+    </div>
+    </>
+);
+
+}
+
+
