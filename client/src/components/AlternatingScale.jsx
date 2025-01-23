7a70d4d7c863ab0974b9d56bca886dd1f22cb3a9
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-23 16:52:17 -0600

    updating Alternating Scale classes

--- client/src/components/AlternatingScale.jsx
+++ client/src/components/AlternatingScale.jsx
@@ -6,20 +6,20 @@ export default function AlternatingScale({}) {
 
   return (
     <>
-    <div className="descendingScale1Container"> 
+    <div className="alternatingScaleContainer"> 
 
-    <div className="ds1Square1 square">1</div>
-    <div className="ds1Square2 square">2</div>
-    <div className="ds1Square3 square">3</div>
-    <div className="ds1Square4 square">4</div> 
-    <div className="ds1Square5 square">5</div>
-    <div className="ds1Square6 square">6</div>
-    <div className="ds1Square7 square">7</div>
-    <div className="ds1Square8 square">8</div>
-    <div className="ds1Square9 square">9</div>
-    <div className="ds1Square10 square">10</div>
-    <div className="ds1Square11 square">11</div>
-    <div className="ds1Square12 square">12</div>
+    <div className="asSquare1 square">1</div>
+    <div className="asSquare2 square">2</div>
+    <div className="asSquare3 square">3</div>
+    <div className="asSquare4 square">4</div> 
+    <div className="asSquare5 square">5</div>
+    <div className="asSquare6 square">6</div>
+    <div className="asSquare7 square">7</div>
+    <div className="asSquare8 square">8</div>
+    <div className="asSquare9 square">9</div>
+    <div className="asSquare10 square">10</div>
+    <div className="asSquare11 square">11</div>
+    <div className="asSquare12 square">12</div>
 
     </div>
 
