25a72db9496d279f2e6e822dc2f4631887eb20cb
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-22 18:34:00 -0600

    mosaic square design and new descending scale file

--- client/src/components/MosaicPage.jsx
+++ client/src/components/MosaicPage.jsx
@@ -8,18 +8,18 @@ export default function MosaicPage({}) {
     <>
     <div className="mosaicContainer"> 
 
-    <div className="mosaicSquare square1">1</div>
-    <div className="mosaicSquare square2">2</div>
-    <div className="mosaicSquare square3">3</div>
-    <div className="mosaicSquare square4">4</div> 
-    <div className="mosaicSquare square5">5</div>
-    <div className="mosaicSquare square6">6</div>
-    <div className="mosaicSquare square7">7</div>
-    <div className="mosaicSquare square8">8</div>
-    <div className="mosaicSquare square9">9</div>
-    <div className="mosaicSquare square10">10</div>
-    <div className="mosaicSquare square11">11</div>
-    <div className="mosaicSquare square12">12</div>
+    <div className="mosaicSquare1 square">1</div>
+    <div className="mosaicSquare2 square">2</div>
+    <div className="mosaicSquare3 square">3</div>
+    <div className="mosaicSquare4 square">4</div> 
+    <div className="mosaicSquare5 square">5</div>
+    <div className="mosaicSquare6 square">6</div>
+    <div className="mosaicSquare7 square">7</div>
+    <div className="mosaicSquare8 square">8</div>
+    <div className="mosaicSquare9 square">9</div>
+    <div className="mosaicSquare10 square">10</div>
+    <div className="mosaicSquare11 square">11</div>
+    <div className="mosaicSquare12 square">12</div>
 
     </div>
 
