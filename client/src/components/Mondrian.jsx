42b00892fb3e983a3e941c71b785879cad2f2556
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-29 16:35:56 -0600

    finished mondrian design

--- client/src/components/Mondrian.jsx
+++ client/src/components/Mondrian.jsx
@@ -6,20 +6,19 @@ export default function Mondrian({}) {
 
   return (
     <>
-    <div className="MondrianContainer"> 
+    <div className="monContainer"> 
 
-    <div className="monSquare1 square">1</div>
-    <div className="monSquare2 square">2</div>
-    <div className="monSquare3 square">3</div>
-    <div className="monSquare4 square">4</div> 
-    <div className="monSquare5 square">5</div>
-    <div className="monSquare6 square">6</div>
-    <div className="monSquare7 square">7</div>
-    <div className="monSquare8 square">8</div>
-    <div className="monSquare9 square">9</div>
-    <div className="monSquare10 square">10</div>
-    <div className="monSquare11 square">11</div>
-    <div className="monSquare12 square">12</div>
+    <div className="monSquare1 monSquare">1</div>
+    <div className="monSquare2 monSquare">2</div>
+    <div className="monSquare3 monSquare">3</div>
+    <div className="monSquare4 monSquare">4</div> 
+    <div className="monSquare5 monSquare">5</div>
+    <div className="monSquare6 monSquare">6</div>
+    <div className="monSquare7 monSquare">7</div>
+    <div className="monSquare8 monSquare">8</div>
+    <div className="monSquare9 monSquare">9</div>
+    <div className="monSquare10 monSquare">10</div>
+
 
     </div>
 
