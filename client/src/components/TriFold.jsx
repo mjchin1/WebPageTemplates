b79c424d0fe1aaceae32cd5af1ece14709f06599
Melissa J. Chin <melissa1jchin@gmail.com>
2025-01-29 15:48:56 -0600

    adding tri-fold design

--- client/src/components/TriFold.jsx
+++ client/src/components/TriFold.jsx
@@ -7,9 +7,9 @@ export default function TriFold({}) {
   return (
     <>
     <div className="tfContainer"> 
-    <div className="tfSquare1 square">1</div> 
-    <div className="tfSquare2 square">2</div>
-    <div className="tfSquare3 square">3</div>
+    <div className="tfSquare1 tfSquare">1</div> 
+    <div className="tfSquare2 tfSquare">2</div>
+    <div className="tfSquare3 tfSquare">3</div>
 
     </div>
     </>
